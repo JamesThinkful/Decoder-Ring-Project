d042641947cc2878f878c01d6ee7e8ffe2bc98d1
JamesThinkful <76189517+JamesThinkful@users.noreply.github.com>
2021-02-03 16:17:42 -0500

    Update caesar.js

--- Project_Decoder_Ring_1/src/caesar.js
+++ Project_Decoder_Ring_1/src/caesar.js
@@ -1,28 +1,19 @@
-function caesar(input, shift, encode = true) {
-  if (!shift || shift === 0 || shift < -25 || shift > 25) return false;
-  if (encode === false) shift = shift * -1;
+function caesar(input, shift, encode = true){
+if(!shift || shift === 0 || shift > 25 || shift < -25) return false;
+let inputArr = input.toLowerCase().split(``)
 
-  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-  input = input.toLowerCase().split("");
-  let result = "";
-
-  let cipher = (input, shift, alphabet) => {
-    let charSwitch = input.forEach((letter) => {
-      if (letter === " ") {
-        result += letter;
-        return result;
-      }
-      let currentIndex = alphabet.indexOf(letter);
-      let newIndex = currentIndex + shift;
-      if (newIndex > 25) newIndex = newIndex - 26;
-      if (newIndex < 0) newIndex = newIndex + 26;
-
-      result += alphabet[newIndex];
-    });
-    return result;
-  };
-
-  return cipher(input, shift, alphabet);
+function isCharacterALetter(char) {
+    return char.toLowerCase() != char.toUpperCase()
+  }
+    for(let ltr in inputArr){
+        if(isCharacterALetter(inputArr[ltr])){
+        let currentLetterCode = inputArr[ltr].charCodeAt(0)
+        encode ? inputArr[ltr] = String.fromCharCode(currentLetterCode + shift) : inputArr[ltr] = String.fromCharCode(currentLetterCode - shift);
+        if (inputArr[ltr].charCodeAt(0) > 122) inputArr[ltr] = String.fromCharCode(inputArr[ltr].charCodeAt(0) - 26);
+        if (inputArr[ltr].charCodeAt(0) < 97) inputArr[ltr] = String.fromCharCode(inputArr[ltr].charCodeAt(0) + 26);
+        }
+    }
+return inputArr.join('')
 }
 
 module.exports = caesar;
