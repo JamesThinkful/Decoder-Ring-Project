a8c91f19e72828eaab30f709abedc4bee242bd3d
JamesThinkful <76189517+JamesThinkful@users.noreply.github.com>
2021-02-03 16:19:48 -0500

    Update substitution.test.js

--- Project_Decoder_Ring_1/test/substitution.test.js
+++ Project_Decoder_Ring_1/test/substitution.test.js
@@ -1,30 +1,47 @@
-const substitution = require("../src/substitution.js");
-const expect = require("chai").expect;
+const { expect } = require('chai');
+const substitution = require('../src/substitution');
 
-describe("substitution", () => {
-  //happy path
-  it("should decode or encode a message with a substitution cipher", () => {
-    const expected = "zyx";
-    const actual = substitution("abc", "zyxwvutsrqponmlkjihgfedcba");
-    expect(actual).to.eql(expected);
-  });
+describe(('Substitution Cipher'), () =>  {
+    it(('Should return an encoded message'), () => {
+        const expected = 'jrufscpw';
+        const actual = substitution("thinkful", "xoyqmcgrukswaflnthdjpzibev");
+        expect(actual).to.eql(expected)
+    })
+    it(('Should return a decoded message'), () => {
+        const expected = 'thinkful';
+        const actual = substitution("jrufscpw", "xoyqmcgrukswaflnthdjpzibev", false);
+        expect(actual).to.eql(expected)
+    })
+    it(('Should return false if input is missing'), () => {
+        const actual = substitution("", "xoyqmcgrukswaflnthdjpzibev");
+        expect(actual).to.be.false;
+    });
+    it(('Should return false if alphabet is missing'), () => {
+        const actual = substitution("woo", "");
+        expect(actual).to.be.false;
+    });
 
-  it("returns false if given alphabet is anything but 26 characters", () => {
-    const lessThanTwentySix = substitution("abc", "zxy");
-    expect(lessThanTwentySix).to.be.false;
-  });
-  it("returns false if there are any duplicate characters in the given alphabet", () => {
-    const dupeChars = substitution("abc", "aacdefghijklmnopqrstuvwxyz");
-    expect(dupeChars).to.be.false;
-  });
-  it("should maintain spaces before and after encoding and decoding", () => {
-    const expected = "zyx zyx";
-    const actual = substitution("abc abc", "zyxwvutsrqponmlkjihgfedcba");
-    expect(actual).to.eql(expected);
-  });
-  it("should ignore capital letters", () => {
-    const expected = "zyx";
-    const actual = substitution("abc", "zyxwvutsrqponmlkjihgfedcba");
-    expect(actual).to.eql(expected);
-  });
-});
+    it(('Should allow special characters in the alphabet'), () => {
+        const actual = substitution("what", "!oyqmcgrukswaflnthdjpzibev");
+        expect(actual).to.eql("ir!j")
+    });
+
+    it(('Should return false if the alphabet parameter is not a string of exactly 26 characters'), () => {
+        const actual = substitution("thinkful", "short");
+        expect(actual).to.be.false
+    })
+    it(('Should return false if each character in the alphabet is not unique'), () => {
+        const actual = substitution("thinkful", "abcabcabcabcabcabcabcabcyz");
+        expect(actual).to.be.false
+    })
+    it(('Spaces should be maintained throughout'), () => {
+        const expected = 'elp xhm xf mbymwwmfj dne';
+        const actual = substitution("You are an excellent spy", "xoyqmcgrukswaflnthdjpzibev");
+        expect(actual).to.eql(expected)
+    })
+    it(('Capital letters can be ignored'), () => {
+        const expected = 'jrufscpw';
+        const actual = substitution("THINKFUL", "xoyqmcgrukswaflnthdjpzibev");
+        expect(actual).to.eql(expected)
+    })
+})
