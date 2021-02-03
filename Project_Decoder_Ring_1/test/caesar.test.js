c183bc93770242fed525f143ad68784fea61afae
JamesThinkful <76189517+JamesThinkful@users.noreply.github.com>
2021-02-03 16:18:59 -0500

    Update caesar.test.js

--- Project_Decoder_Ring_1/test/caesar.test.js
+++ Project_Decoder_Ring_1/test/caesar.test.js
@@ -1,50 +1,48 @@
-const caesar = require("../src/caesar.js");
-const expect = require("chai").expect;
+const { expect } = require("chai");
+const caesar = require("../src/caesar");
 
-describe("caesar", () => {
-  //Happy path
-  it("should decode or encode a message with a caesar cipher", () => {
-    const expected = "def";
-    const actual = caesar("abc", 3);
-    expect(actual).to.eql(expected);
-  });
-
-  /*It returns false if the shift value is equal to 0, 
-    less than -25, greater than 25, or not present.*/
-
-  it("should return false if shift value is incorrect or not present", () => {
-    const equalsZero = caesar("abc", 0);
-    const lessThanNeg25 = caesar("abc", -36);
-    const greaterThan25 = caesar("abc", 26);
-    const notPresent = caesar("abc");
-
-    expect(equalsZero).to.be.false;
-    expect(lessThanNeg25).to.be.false;
-    expect(greaterThan25).to.be.false;
-    expect(notPresent).to.be.false;
-  });
-
-  // It should ignore capital letters
-
-  it("should ignore capital letters", () => {
-    const expected = "def";
-    const actual = caesar("ABC", 3);
-    expect(actual).to.eql(expected);
-  });
-
-  /* When encoding, it handles shifts that go past the end of the alphabet. 
-     (For example, shifting “z” to the right by 3 should cause the “z” to wrap around 
-     to the front of the alphabet, so that “z” becomes “c”)*/
-
-  it("should handle shifts that go past the end of the alphabet", () => {
-    const expected = "abc";
-    const actual = caesar("xyz", 3);
-    expect(actual).to.eql(expected);
-  });
-
-  it("should maintain spaces before and after encoding", () => {
-    const expected = "def def";
-    const actual = caesar("abc abc", 3);
-    expect(expected).to.eql(actual);
-  });
-});
+describe(('Caesar Shift'),() => {
+    it(('Should return an encoded message'), () => {
+        const expected = "wklqnixo";
+        const actual = caesar("Thinkful", 3)
+        expect(actual).to.eql(expected); 
+    })
+    it(('Should return a decoded message'), () => {
+        const expected = "thinkful";
+        const actual = caesar("wklqnixo", 3, false)
+        expect(actual).to.eql(expected); 
+    })
+    it(('Should return false if input or shift value is missing'), () => {
+        const actual = caesar("", );
+        expect(actual).to.be.false;
+    })
+    it(('Should return false if shift value is equal to 0'), () => {
+        const actual = caesar("Thinkful", 0);
+        expect(actual).to.be.false;
+    })
+    it(('Should return false if shift value is equal to 25'), () => {
+        const expected = false;
+        const actual = caesar("Thinkful", 30);
+        expect(actual).to.eql(expected); 
+    })
+    it(('Should return false if shift value is equal to 25'), () => {
+        const expected = false;
+        const actual = caesar("Thinkful", -30);
+        expect(actual).to.eql(expected); 
+    })
+    it(('Spaces, and other non-alphabetic symbols, should be maintained throughout'), () => {
+        const expected = "this is a secret message!";
+        const actual = caesar("BPQA qa I amkzmb umaaiom!", 8, false);
+        expect(actual).to.eql(expected); 
+    })
+    it(('Capital letters can be ignored.'), () => {
+        const expected = "mfuud sjb djfw";
+        const actual = caesar("HAPPY NEW YEAR", 5);
+        expect(actual).to.eql(expected); 
+    })
+    it(('Letters shifted off the alphabet should wrap around'), () => {
+        const expected = "abcde";
+        const actual = caesar("VWXYZ", 5)
+        expect(actual).to.eql(expected); 
+    })
+})
