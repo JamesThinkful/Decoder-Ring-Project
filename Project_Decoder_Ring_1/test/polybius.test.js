c076abf9aed20ec5a376ea7c638d565fe364bf77
JamesThinkful <76189517+JamesThinkful@users.noreply.github.com>
2021-02-03 16:19:17 -0500

    Update polybius.test.js

--- Project_Decoder_Ring_1/test/polybius.test.js
+++ Project_Decoder_Ring_1/test/polybius.test.js
@@ -1,39 +1,38 @@
-const polybius = require("../src/polybius.js");
-const expect = require("chai").expect;
+const { expect } = require("chai");
+const polybius = require("../src/polybius");
 
-describe("polybius", () => {
-  //happy path
-  it("should encode and decode using a polybius cipher", () => {
-    const expected = "agn";
-    const actual = polybius("112233", false);
-
-    expect(actual).to.eql(expected);
-  });
-
-  //Translate "42" to "i/j" and "i/j" to "42"
-  it("should translate '42' to i/j", () => {
-    const fortyTwoToIAndJ = "i/j";
-    const fortyTwoactual = polybius("42", false);
-    const IandJToFortyTwo = "254233";
-    const IandJActual = polybius("wi/jn", true);
-    expect(fortyTwoactual).to.eql(fortyTwoToIAndJ);
-    expect(IandJActual).to.eql(IandJToFortyTwo);
-  });
-
-  //ignores capital letters
-  it("should ignore capital letters", () => {
-    const expected = "112131";
-    const actual = polybius("ABC");
-    expect(actual).to.eql(expected);
-  });
-
-  //maintains spaces
-  it("should maintain spaces", () => {
-    const expected = "11 22 33";
-    const actual = polybius("a g n");
-  });
-  //checks if odd number of characters when decoding
-  it("should have an even number of characters when decoding", () => {
-    expect(polybius("12133", false)).to.be.false;
-  });
+describe(('Polybius Square'),() => {
+    it(('Should return an encoded message'), () => {
+        const expected = "4432423352125413";
+        const actual = polybius("thinkful");
+        expect(actual).to.eql(expected);
+    });
+    it(('Should return a decoded message'), () => {
+        const expected = "th(i/j)nkful";
+        const actual = polybius("4432423352125413", false);
+        expect(actual).to.eql(expected);
+    });
+    it(('Should return false if input is missing'), () => {
+        const actual = polybius("", false);
+        expect(actual).to.be.false
+    });
+    it(('Should return false if the string of coordinates is not an even number, ignoring spaces'), () => {
+        const actual = polybius("3251131343 25432413419", false);
+        expect(actual).to.be.false
+    });
+    it(('Spaces should be maintained throughout'), () => {
+        const expected = "hello world";
+        const actual = polybius("3251131343 2543241341", false);
+        expect(actual).to.eql(expected);
+    });
+    it(('Capital letter should be ignored'), () => {
+        const expected = "3251131343 2543241341";
+        const actual = polybius("Hello World");
+        expect(actual).to.eql(expected);
+    });
+    it(('The letters "I" and "J" should be shown together if included'), () => {
+        const expected = "th(i/j)nkful";
+        const actual = polybius("4432423352125413", false);
+        expect(actual).to.eql(expected);
+    });
 });
